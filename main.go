cb1ab00f0b8d4805980d800f1df453a0373b9fcb
Julian Cantillo <umbrasoft@gmail.com>
2016-07-26 00:19:40 -0500

    Started local server for contact book

---
+++ main.go
@@ -0,0 +1,29 @@
+// Web Server
+
+package main
+
+import (
+	"fmt"
+	"github.com/boltdb/bolt"
+	"log"
+	"net/http"
+)
+
+func db() {
+	// Open the my.db data file in your current directory.
+	// It will be created if it doesn't exist.
+	db, err := bolt.Open("my.db", 0600, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
